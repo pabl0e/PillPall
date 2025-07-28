d644a3118117272dead2319998fe512bad220fc7
pabl0e <vincelouievalle@gmail.com>
2025-07-28 14:47:39 +0800

    Landing Page Cards
    -Added padding to Doctor's , Tasks, and Symptom cards

--- lib/views/landing_page.dart
+++ lib/views/landing_page.dart
@@ -622,6 +622,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ],
                 ),
+                ),
               ],
             ),
           ),
